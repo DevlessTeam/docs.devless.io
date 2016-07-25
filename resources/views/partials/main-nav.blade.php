ab928695b336a0617b8449a496555ac5f6369a3b
jlsodai <jlsodai@gmail.com>
2016-07-25 13:57:51 +0000

    add nav menu items

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -1,33 +1,18 @@
-<li class="nav-docs"><a href="/docs">Documentation</a></li>
-<!-- 
-<li class="dropdown community-dropdown">
-	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Services <span class="caret"></span></a>
-	<ul class="dropdown-menu" role="menu">
-		<li class="nav-forge"><a href="https://forge.laravel.com">Forge</a></li>
-		<li class="nav-envoyer"><a href="https://envoyer.io">Envoyer</a></li>
-	</ul>
-</li>
-
-<li class="dropdown community-dropdown">
-	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Conference <span class="caret"></span></a>
-	<ul class="dropdown-menu" role="menu">
-		<li><a href="http://laracon.us">Laracon US</a></li>
-		<li><a href="http://laracon.eu">Laracon EU</a></li>
-	</ul>
-</li>
+<li><a href="#">API</a></li>
+<li><a href="#">API Specs</a></li>
+<li><a href="#">Console</a></li>
+<li><a href="#">Philosophy</a></li>
+<li><a href="#">SDK</a></li>
+<li><a href="#">Tutorials</a></li>
 
 <li class="dropdown community-dropdown">
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Community <span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
-		<li><a href="https://github.com/laravel/laravel">GitHub</a></li>
-		<li class="divider"></li>
-		<li><a href="http://blog.laravel.com">Laravel Blog</a></li>
-		<li class="divider"></li>
-		<li><a href="https://larajobs.com/?partner=5#/">Jobs</a></li>
-		<li><a href="https://laracasts.com/discuss">Forums</a></li>
-		<li><a href="https://laravel-news.com/">Newsletter</a></li>
-		<li><a href="http://www.laravelpodcast.com/">Podcast</a></li>
-		<li><a href="http://larachat.co">Slack</a></li>
-		<li><a href="https://twitter.com/laravelphp">Twitter</a></li>
+		<li><a href="#">Jobs</a></li>
+		<li><a href="#">Forums</a></li>
+		<li><a href="#">Newsletter</a></li>
+		<li><a href="#">Podcast</a></li>
+		<li><a href="#">Slack</a></li>
+		<li><a href="#">Twitter</a></li>
 	</ul>
-</li> -->
+</li>
